d6d4398fe4e5209d08a8ddc362b8de83a905a90f
Mateo Contenla <mcontenlaf@gmail.com>
2017-10-03 20:33:42 -0300

    Muchas mejoras y limpieza de código, aunque falta que elimine el archivo 'hosts_temp' al finalizar su ejecución

--- bloqueador.py
+++ bloqueador.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from datetime import timedelta
-from sys import argv
+import sys
 import re
 import os
 
@@ -10,18 +10,18 @@ Programa que bloquea sitios por determinada cantidad de tiempo
 
 La lista de sitios a bloquear se encuentra en el archivo \"Páginas Web.txt\".
 
-blqueador.py [hr][m][s]
+Uso: blqueador.py [hr][m][s]
 
 Comandos:
 
     [hr]    Cantidad de horas en el temporizador
-    [m]     Cantidad de segundos en el temporizador
+    [m]     Cantidad de minutos en el temporizador
     [s]     Cantidad de segundos en el temporizador
-    
+
 
 Ejemplos:
- 
-    bloqueador.py 3hr4m6s        Bloquea los sitios por 3 horas, 4 minutos y 6 segundos 
+
+    bloqueador.py 3hr4m6s        Bloquea los sitios por 3 horas, 4 minutos y 6 segundos
 
     bloqueador.py 2hr            Bloquea los sitios por 2 horas
 
@@ -29,8 +29,7 @@ Ejemplos:
 '''
 
 
-##### Función que parsea input de tiempo y lo transforma en un objecto timedelta #####
-
+# Función que parsea input de tiempo y lo transforma en un objecto timedelta
 def syntax_tiempo(cadena_tiempo, patron_compilado):
     partes = patron_compilado.match(cadena_tiempo)
     if partes:
@@ -41,13 +40,13 @@ def syntax_tiempo(cadena_tiempo, patron_compilado):
                 parametros_tiempo[nombre] = int(parametros)
         return timedelta(**parametros_tiempo)
     else:
-        raise SyntaxError
+        print(msje_ayuda)
+        sys.exit()
 
 
-##### Función que devuelve un archivo a su estado original basándose en una copia temporal #####
-    
-def volver_original(original, copia, mensaje=""):
-    print(mensaje)
+# Función que devuelve un archivo a su estado original basándose en una copia temporal
+def volver_original(original, copia):
+    print("¡Ahora eres libre de nuevo!")
     with open(copia, "r") as temp:
         lineas = temp.readlines()
         with open(original, "w") as hosts_or:
@@ -55,10 +54,9 @@ def volver_original(original, copia, mensaje=""):
                 hosts_or.writelines(linea)
             hosts_or.close()
         temp.close()
-    
 
-##### Clase que crea archivo temporales y los elimina #####
-        
+
+# Clase que crea archivo temporales y los elimina
 class Copia:
     def __init__(self, archivo):
         self.ruta = archivo
@@ -73,73 +71,47 @@ class Copia:
             original.close()
 
     def eliminar(self):
+        """Elimina la copia del archivo"""
         os.remove(self.copia)
 
 
 def main():
-
     delta_tiempo = None
-
+    # NOTE: Agregar detección de OS
     ruta_hosts = r"C:\Windows\System32\drivers\etc\hosts"
-
     redireccion = "127.0.0.1"
-
+    # Hora de comienzo de script
     hora_com = datetime.now()
+    patron_tiempo = re.compile(r"((?P<hours>\d+?)hr)?((?P<minutes>\d+?)m)?((?P<seconds>\d+)s)?")
 
-    patron_tiempo = re.compile(r"((?P<hours>\d+?)hr)?((?P<minutes>\d+?)m)?((?P<seconds>\d+?)s)+")
+    try:
+        delta_tiempo = syntax_tiempo(sys.argv[1], patron_tiempo)
 
-    if len(argv) > 1:
+        print("Tiempo: ", delta_tiempo)
+        hora_fin = hora_com + delta_tiempo
 
-        if argv[1] != "--help":
-            
-            try:
-                try:
-                    delta_tiempo = syntax_tiempo(argv[1], patron_tiempo)
-                except Exception as e:
-                    print("Ejecuta el comando \"python bloqueador.py --help\" para obtener ayuda.")
-                    exit()
+        hosts = Copia(ruta_hosts)
+        hosts.copiar()
 
-                print(delta_tiempo)
-                hora_fin = hora_com + delta_tiempo
+        # Añadir páginas al archivo 'hosts'
+        with open("Páginas Web.txt", "r") as pag:
+            paginas = [linea.rstrip("\n") for linea in pag.readlines()]
+        with open(ruta_hosts, "a") as hosts_arch:
+            for pagina in paginas:
+                hosts_arch.write("\n{} {}".format(redireccion, pagina))
 
-                hosts = Copia(ruta_hosts)
-                hosts.copiar()
+        # Checkear constantemente si ya se llegó a la hora final
+        while True:
+            if datetime.now() > hora_fin:
+                volver_original(ruta_hosts, hosts.copia)
+                return
 
-                ##### Añadir páginas al archivo "hosts" #####
-                with open("Páginas Web.txt", "r") as pag:
-                    paginas = [linea.rstrip("\n") for linea in pag.readlines()]
-                    pag.close()
-                with open(ruta_hosts, "a") as hosts_arch:
-                    for pagina in paginas:
-                        hosts_arch.write("\n{} {}".format(redireccion, pagina))
-                    hosts_arch.close()
+    except KeyboardInterrupt:
+        volver_original(ruta_hosts, hosts.copia)
 
-                ##### Checkear constantemente si ya se llegó a la hora final #####  
-                while True:
-                    if datetime.now() > hora_fin:
-                        print("wena prro")
-                        with open(hosts.copia, "r") as temp:
-                            lineas = temp.readlines()
-                            with open(ruta_hosts, "w") as hosts_or:
-                                for linea in lineas:
-                                    hosts_or.writelines(linea)
-                                hosts_or.close()
-                            temp.close()
-
-                        hosts.eliminar()
-                        break
-                    
-            except KeyboardInterrupt:
-                volver_original(ruta_hosts, hosts.copia, mensaje="¡Ahora eres libre!")
-                    
-            elif argv[1] == "--help":
-                print(msje_ayuda)
-        
-        else:
-            print("Ejecuta el comando \"python bloqueador.py --help\" para obtener ayuda.")
-
-    else:
-        print("Ejecuta el comando \"python bloqueador.py --help\" para obtener ayuda.")
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print(msje_ayuda)
+        sys.exit()
     main()
